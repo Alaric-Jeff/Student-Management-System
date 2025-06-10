dbfe315a7c5e1a67c0b18e588f1c497033b57201
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-06-11 02:22:39 +0800

    added plugins, and updated services

--- backend/src/services/StudentServices/addStudent.ts
+++ backend/src/services/StudentServices/addStudent.ts
@@ -4,7 +4,24 @@ import isExisting from "./isExisting.js";
 import bcrypt from "bcryptjs";
 
 async function addStudentService(prisma: PrismaClient) {
-    return async function addStudent() {
-        
+    return async function addStudent(
+        id: number, email: string, password: string, firstName: string, lastName: string, middleName: string
+    ){
+        try{
+            
+        }catch(err: unknown){
+            if(err instanceof Error){
+                logger.error("Error occured in add student ", err.message)
+                throw new Error("Error in adding student")
+            }else{
+                logger.error("Unknown error occured in adding the student: ", err)
+                throw new Error("Unknown error occured in adding the student")
+            }
+        }
     }
 }
+
+export default addStudentService;
+
+
+
